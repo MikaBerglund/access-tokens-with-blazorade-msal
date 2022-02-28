02c430e8af95ebb8c500ce625f322945f2e1ee2b
Mika Berglund <mika.berglund@outlook.com>
2022-02-28 19:16:39 +0200

    Added reference to Blazorade MSAL and building the sample application.

--- GraphClientSample/Program.cs
+++ GraphClientSample/Program.cs
@@ -1,3 +1,4 @@
+using Blazorade.Msal.Configuration;
 using GraphClientSample;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -6,6 +7,18 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddBlazoradeMsal((sp, options) =>
+    {
+        var config = sp.GetRequiredService<IConfiguration>();
+        var appSection = config.GetSection("app");
+        appSection.Bind(options);
+
+        options.InteractiveLoginMode = InteractiveLoginMode.Popup;
+        options.TokenCacheScope = TokenCacheScope.Persistent;
+        options.PostLogoutUrl = "/loggedout";
+    })
+    ;
 
 await builder.Build().RunAsync();
